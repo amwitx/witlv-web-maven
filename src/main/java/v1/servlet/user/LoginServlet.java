b5fccfccf7ee3cd85840ebaeca8a91afd778b2d4
tearers <tearers@qq.com>
2017-01-01 22:09:46 +0800

    propertoies

--- src/main/java/v1/servlet/user/LoginServlet.java
+++ src/main/java/v1/servlet/user/LoginServlet.java
@@ -1,6 +1,11 @@
 package v1.servlet.user;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,29 +14,59 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-public class LoginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: login").append(request.getContextPath());
+	public LoginServlet()
+	{
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: login").append(request.getContextPath());
+
+		response.setHeader("content-type", "text/html;charset=utf-8");
+		ServletContext context = this.getServletContext();
+
+		try
+		{
+			Properties props = new Properties();
+			InputStream stream = context.getResourceAsStream("/WEB-INF/classes/v1/db.properties");
+			props.load(stream);
+			// String proPath =
+			// context.getRealPath("WEB-INF/config//db.properties");
+			// props.load(new FileInputStream(proPath));
+			String driver = props.getProperty("jdbc.driver");
+			String url = props.getProperty("jdbc.url");
+			String username = props.getProperty("jdbc.username");
+			String password = props.getProperty("jdbc.password");
+
+			response.getWriter().append(MessageFormat.format("driver={0},url={1},username={2},password{3}", driver, url,
+					username, password));
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+			response.getWriter().append(e.getMessage());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
